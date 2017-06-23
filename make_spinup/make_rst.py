f4bf4fb2e812e7261101054e2b85fadb389909f8
Chuning Wang <chuning@esm.rutgers.edu>
2017-06-23 19:26:57 -0400

    change time step for restart file. Make the restart file always read 2000/01/03.

--- make_spinup/make_rst.py
+++ make_spinup/make_rst.py
@@ -3,5 +3,13 @@ import numpy as np
 
 rstfile = '/glade/scratch/chuning/tmpdir_GB-SPINUP/GB-SPINUP_rst.nc'
 fh = nc.Dataset(rstfile)
-fh.variables['ocean_time'][0] = fh.variables['ocean_time'][1]+24*60*60
+t0 = fh.variables['ocean_time'][0]
+t1 = fh.variables['ocean_time'][1]
+if t0>t1:
+    fh.variables['ocean_time'][0] = 36526*24*60*60
+    fh.variables['ocean_time'][1] = fh.variables['ocean_time'][0]-24*60*60
+else:
+    fh.variables['ocean_time'][1] = 36526*24*60*60
+    fh.variables['ocean_time'][0] = fh.variables['ocean_time'][0]-24*60*60
+
 fh.close()
