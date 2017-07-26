bd0652025e595e25abeed1b65da1e7f655bf82ee
Chuning Wang <chuning@esm.rutgers.edu>
2017-07-26 10:05:43 -0400

    minor fix

--- make_clm/make_yearly_clim.bash
+++ make_clm/make_yearly_clim.bash
@@ -4,11 +4,11 @@ fyear=2000
 lyear=2000
 
 grd_name=GlacierBay_usgs
-dst_dir=/Volumes/R1/scratch/chuning/gb_roms/data/clim/
+dst_dir=/Volumes/R1/scratch/chuning/gb_roms/clm/
 
 for year in $(seq $fyear $lyear ) ; do
 
-    #python make_clim_file.py $year $dst_dir
+    python make_clim_file.py $year $dst_dir
 
     cd $dst_dir 
     ncrcat -O ${grd_name}_clim_${year}_??_SODA3.3.1.nc -o ${grd_name}_clim_${year}_SODA3.3.1.nc
