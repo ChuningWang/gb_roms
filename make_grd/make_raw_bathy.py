ec2430dbd7bb5274773fc32b01f500e4e71831c6
Chuning Wang <chuning@esm.rutgers.edu>
2017-08-01 11:38:54 -0400

    add more survey segments.

--- make_grd/make_raw_bathy.py
+++ make_grd/make_raw_bathy.py
@@ -43,6 +43,33 @@ lon = np.array(lon)
 lat = np.array(lat)
 z = np.array(z)
 
+# special treatment for Adams Inlet
+lat_ai = []
+lon_ai = []
+z_ai = []
+f = open(bathy_dir+'bathy/adams_inlet/surveys.xyz', 'rb')
+rd = csv.reader(f, delimiter='\t')
+next(rd, None)
+for row in rd:
+    lon_ai.append(float(row[1]))
+    lat_ai.append(float(row[2]))
+    z_ai.append(float(row[3]))
+
+lon_ai = np.array(lon_ai)
+lat_ai = np.array(lat_ai)
+z_ai = np.array(z_ai)
+
+lon1 = -136.
+lon2 = -135.75
+msk = (lon_ai>lon1) & (lon_ai<lon2)
+lon_ai = lon_ai[msk]
+lat_ai = lat_ai[msk]
+z_ai = z_ai[msk]
+
+lon = np.concatenate((lon, lon_ai), axis=0)
+lat = np.concatenate((lat, lat_ai), axis=0)
+z = np.concatenate((z, z_ai))
+
 fh = nc.Dataset(bathy_dir+'bathy_noaa.nc', 'w')
 fh.createDimension('ct')
 fh.createVariable('lon', 'd', ('ct'))
